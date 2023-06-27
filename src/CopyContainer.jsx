02abe9d898c7cef988ffd3f5deb40487d0cb3cc7
Adam Zakaria <adam.p.zakaria@gmail.com>
2023-06-26 22:28:56 -0400

    s

--- src/CopyContainer.jsx
+++ src/CopyContainer.jsx
@@ -29,8 +29,8 @@ function Text(props) {
 
   return (
     <pre className='rounded-b-md'>
-      <code ref={codeRef} className={`rounded-b-md language-${props.lang}`}>
-        <div className='text-body p-[12px] rounded-b-md inner'>
+      <code ref={codeRef} className={`text-body rounded-b-md language-${props.lang}`}>
+        <div className='p-[12px] rounded-b-md inner'>
           {props.children}
         </div>
       </code>
