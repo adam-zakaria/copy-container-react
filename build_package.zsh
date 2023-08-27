7c49e1f711035ded373cd2a9b0ed7196052ff5b1
Adam Zakaria <adam.p.zakaria@gmail.com>
2023-08-27 15:47:10 -0400

    Update build_package

--- build_package.zsh
+++ build_package.zsh
@@ -1,6 +1,4 @@
 # to run
-# source build_package.zsh
 g 's'
-npm run babel
-npx tailwindcss -i ./src/App.css -o ./src/predist/App.css; npx tailwindcss -i ./src/CopyContainer.css -o ./src/predist/CopyContainer.css
+rollup -c
 npm version patch; npm publish
